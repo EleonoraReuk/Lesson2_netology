f549c9a750800f1b223c3a34f901311e7c11a186
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-02-14 11:40:47 +0700

    Add files via upload

---
+++ Lesson2Netology.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	int x;
+	std::cout << "Введите число: " << std::endl;
+	std::cin >> x;
+	std::cout << "Вы ввели: " << std::endl;
+	std::cout << x << std::endl;
+
+}
---
+++ Lesson2NetologyPart2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::string x;
+	std::cout << "Введите слово:" << std::endl;
+	std::cin >> x;
+	std::cout << "Вы ввели:" << std::endl;
+	std::cout << x;
+}
